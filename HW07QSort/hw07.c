19476b6fa051f49c7d873ed3ec7dd0d98b433616
Dorien Penebacker <dpenebac@ecegrid-thin3.ecn.purdue.edu>
2020-09-22 17:50:30 -0400

    completed hw07

--- HW07QSort/hw07.c
+++ HW07QSort/hw07.c
@@ -5,10 +5,28 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h> 
 
 #ifdef TEST_COUNTINT
 int countInt(char * filename)
 {
+  FILE *file;
+  file = fopen(filename,"r");
+  int numNums = 0;
+  int temp;
+
+  if (file == NULL)
+  {
+    printf("Error in file opening");
+    return(-1);
+  }
+
+  while(fscanf(file,"%d",&temp) == 1)
+  {
+    numNums++;
+  }
+
+  fclose(file);
   // count the number of integers in the file
   // Please notice that if a file contains
   // 124 378 -56
@@ -18,6 +36,7 @@ int countInt(char * filename)
   // If fopen fails, return -1
 
   // remember to fclose if fopen succeeds
+  return(numNums);
 }
 #endif
 
@@ -26,13 +45,35 @@ bool readInt(char* filename, int * intArr, int size)
 {
   // if fopen fails, return false
   // read integers from the file.
-  // 
-  //
+
+  FILE *file;
+  file = fopen(filename,"r");
+  int numNums = 0;
+  int temp;
+  int i = 0;
+
+  if (file == NULL)
+  {
+    printf("Error in file opening");
+    return(-1);
+  }
+
+  while(fscanf(file,"%d",&temp) == 1)
+  {
+    numNums++;
+    intArr[i] = temp;
+    i++;
+  }
+
+  if (numNums != size)
+  {
+    return(false);
+  }
   // if the number of integers is different from size (too
   // few or too many) return false
   // 
   // if everything is fine, fclose and return true
-
+  fclose(file);
   return true;
 }
 #endif
@@ -45,6 +86,7 @@ int compareInt(const void *p1, const void *p2)
   // return an integer less than, equal to, or greater than zero if
   // the first argument is considered to be respectively less than,
   // equal to, or greater than the second.
+  return( *(int*)p1 - *(int*)p2);
 }
 #endif
 
@@ -54,7 +96,23 @@ bool writeInt(char* filename, int * intArr, int size)
   // if fopen fails, return false
   // write integers to the file.
   // one integer per line
-  // 
+  FILE *file;
+  file = fopen(filename,"w");
+  int i;
+
+  if (file == NULL)
+  {
+    printf("Error in file opening");
+    return(false);
+  }
+
+  for (i = 0; i < size; i++)
+  {
+    fprintf(file,"%d\n",intArr[i]);
+  }
   // fclose and return true
+
+  fclose(file);
+  return(true);
 }
 #endif
--- HW07QSort/main.c
+++ HW07QSort/main.c
@@ -15,15 +15,19 @@ int main(int argc, char * * argv)
   // argv[2]: name of output file
 
   // if argc is not 3, return EXIT_FAILURE
+  if (argc != 3)
+  {
+    return EXIT_FAILURE;
+  }
 
   // count the number of integers in the file
   int numElem = 0;
   numElem = countInt(argv[1]);
 
   if (numElem == -1) // fopen fails
-    {
-      return EXIT_FAILURE;
-    }
+  {
+    return EXIT_FAILURE;
+  }
 
   // allocate memory for the integers in the file
   // 1. create a pointer variable
@@ -33,27 +37,35 @@ int main(int argc, char * * argv)
 
   int * intArr;
 
+  intArr = (int *)malloc(1000);
+
+  if (intArr == NULL)
+  {
+    return EXIT_FAILURE;
+  }
+
   bool rtv = readInt(argv[1], intArr, numElem);
 
   if (rtv == false) //if read fails, return EXIT_FAILURE
-    { 
-
-    }
+  { 
+    return EXIT_FAILURE;
+  }
   
   // call qsort using the comparison function you write
-  qsort(....);
+  qsort(intArr, numElem, sizeof(int), compareInt);
 
   // write the sorted array to a file whose name is argv[2]
   
   rtv = writeInt(argv[2], intArr, numElem);
   if (rtv == false) // read fail
-    {
+  {
       // release memory
-      return EXIT_FAILURE;
-    }
+    free(intArr);
+    return EXIT_FAILURE;
+  }
 
   // everything is ok, release memory, return EXIT_SUCCESS
-
+  free(intArr);
   return EXIT_SUCCESS;
 }
 #endif
